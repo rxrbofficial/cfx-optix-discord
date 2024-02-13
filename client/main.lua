08de25ea89b7f44c1f8645d2e3003eb600e937a9
RB <142269354+rxrbofficial@users.noreply.github.com>
2024-02-14 04:02:29 +0500

    Create main.lua

---
+++ client/main.lua
@@ -0,0 +1,55 @@
+local jobGrade = ''
+local job = ''
+local playerName = nil
+local playerLoaded = false
+local firstSpawn = true
+-- ESX Stuff----
+ESX = exports["es_extended"]:getSharedObject()
+
+RegisterNetEvent('esx:playerLoaded')
+AddEventHandler('esx:playerLoaded', function(xPlayer)
+	ESX.PlayerData = xPlayer
+	playerLoaded = true
+end)
+
+AddEventHandler('playerSpawned', function()
+ if firstSpawn then
+    for _, v in pairs(Config.Buttons) do
+          SetDiscordRichPresenceAction(v.index, v.name, v.url)
+    end
+    firstSpawn = false
+ end	
+end)
+
+RegisterNetEvent('Boost-Discord:SetPresence')
+AddEventHandler('Boost-Discord:SetPresence', function(data)
+	local data = data
+	local player = PlayerId()
+	if Config.UseJobs then
+		SetDiscordRichPresenceAssetSmall(data['Job'].jobName)
+		SetDiscordRichPresenceAssetSmallText(data['Job'].jobLabel .. " - " .. data['Job'].jobGrade)	
+	end	
+	--This is the Application ID (Replace this with you own)
+	SetDiscordAppId(Config.ClientID)
+	--Here you will have to put the image name for the "large" icon.
+	SetDiscordRichPresenceAsset('presence') --Main Logo Name 
+	SetDiscordRichPresenceAssetSmall('verified')  -- Small Badge Text
+	if Config.UseESXIdentity then
+		SetRichPresence((Config.RichPresence):format(GetPlayerServerId(player), data['IdentityName'], Config.PlayerText, data['ActivePlayers'], tostring(Config.PlayerCount)))
+	else
+		SetRichPresence((Config.RichPresence):format(GetPlayerServerId(player), data['PlayerName'], Config.PlayerText, data['ActivePlayers'], tostring(Config.PlayerCount)))
+	end
+	SetDiscordRichPresenceAssetText('Change Me') -- Main Logo Text
+	SetDiscordRichPresenceAssetSmallText('Badge Text') -- Badge Text
+end)
+
+Citizen.CreateThread(function()
+	while not playerLoaded do
+		Citizen.Wait(10)
+	end
+	while true do
+		TriggerServerEvent('Boost-Discord:UpdatePresence')
+		Citizen.Wait(Config.ResourceTimer*1000)
+	end
+end)
+
